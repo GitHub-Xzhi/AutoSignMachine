c3313f36eeea9484e2dc6c9bf3aa2d320d7d7690
lunnlew <lunnlew@gmail.com>
2020-12-28 13:40:17 +0800

    bilibili优化错误

--- commands/tasks/bilibili/init.js
+++ commands/tasks/bilibili/init.js
@@ -6,7 +6,7 @@ module.exports = (axios) => {
     }).then(res => {
       let result = res.data
       if (result.code !== 0 || !result.data.isLogin) {
-        reject(result.message)
+        throw new Error(result.message)
       } else {
         let userInfo = result.data
         console.log(`获取用户状态成功 用户名：%s，硬币余额：%s，当前等级Lv%s，经验值为：%s，下级经验值为：%s`, userInfo.uname.substr(0, 2) + "********", userInfo.money,userInfo.level_info.current_level, userInfo.level_info.current_exp, userInfo.level_info.next_exp)
--- commands/tasks/bilibili/watchAndShareVideo.js
+++ commands/tasks/bilibili/watchAndShareVideo.js
@@ -15,7 +15,7 @@ var fetchVideo = (axios) => {
       if (result.code === 0) {
         resolve(result.data[Math.floor(Math.random() * result.data.length)])
       } else {
-        resolve()
+        throw new Error('获取随机视频失败')
       }
     }).catch(reject)
   })
