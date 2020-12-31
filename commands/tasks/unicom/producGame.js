377d9df09053cd1527cc5e940a6959d815e74b76
lunnlew <lunnlew@gmail.com>
2020-12-31 22:54:20 +0800

    update

--- commands/tasks/unicom/producGame.js
+++ commands/tasks/unicom/producGame.js
@@ -142,6 +142,30 @@ module.exports = {
             console.log('记录失败')
         }
     },
+    popularGames: async (axios, options) => {
+        const useragent = `Mozilla/5.0 (Linux; Android 7.1.2; SM-G977N Build/LMY48Z; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36; unicom{version:android@8.0100,desmobile:${options.user}};devicetype{deviceBrand:samsung,deviceModel:SM-G977N};{yw_code:}    `
+        let params = {
+            'methodType': 'popularGames',
+            'deviceType': 'Android',
+            'clientVersion': '8.0100',
+        }
+        let { data } = await axios.request({
+            baseURL: 'https://m.client.10010.com/',
+            headers: {
+                "user-agent": useragent,
+                "referer": "https://img.client.10010.com",
+                "origin": "https://img.client.10010.com"
+            },
+            url: `/producGameApp`,
+            method: 'post',
+            data: transParams(params)
+        })
+        if (data) {
+            return data.popularList || []
+        } else {
+            console.log('记录失败')
+        }
+    },
     recordGame1: async (axios, options) => {
         const { gameId } = options
         const useragent = `Mozilla/5.0 (Linux; Android 7.1.2; SM-G977N Build/LMY48Z; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36; unicom{version:android@8.0100,desmobile:${options.user}};devicetype{deviceBrand:samsung,deviceModel:SM-G977N};{yw_code:}    `
@@ -219,10 +243,75 @@ module.exports = {
         })
         if (data) {
             console.log(data.msg)
-            return data.data.filter(g => g.state === '0')
+            return data.data.filter(g => g.state === '0')//0未进行 state=1待领取 state=2已完成
         } else {
             console.log('记录失败')
         }
+    },
+    gameReportTime: async (axios, options) => {
+        // TODO 上报时间-似乎通过q.qq.com的渠道~暂无法处理
+        const { game } = options
+        const useragent = `Mozilla/5.0 (Linux; Android 7.1.2; SM-G977N Build/LMY48Z; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36; unicom{version:android@8.0100,desmobile:${options.user}};devicetype{deviceBrand:samsung,deviceModel:SM-G977N};{yw_code:}    `
+        // let params = {
+        //     "methodType": "1",
+        //     "mobile": options.user,
+        //     "param": {
+        //         "id": game.gameCode,
+        //         "title": game.name,
+        //         "desc": game.gameDesc,
+        //         "dataType": "5",
+        //         "images": game.smallImage,
+        //         "linkUrl": game.url,
+        //         "deviceType": "Android",
+        //         "gameType": "Y"
+        //     }
+        // }
+        // console.log(params)
+        // let { data } = await axios.request({
+        //     baseURL: 'https://m.client.10010.com/',
+        //     headers: {
+        //         "user-agent": useragent,
+        //         "referer": "https://img.client.10010.com",
+        //         "origin": "https://img.client.10010.com"
+        //     },
+        //     url: `/finderInterface/myCircleForEntertainment`,
+        //     method: 'post',
+        //     data: params
+        // })
+        // console.log(data)
+        let i = 40
+        do {
+            console.log(i)
+            let params = [
+                game.gameCode,
+                'https://tga-data-upload.hrgame.com.cn/sync_xcx',
+                1,
+                200,
+                82,
+                10,
+                'tga-data-upload.hrgame.com.cn',
+                'https://tga-data-upload.hrgame.com.cn/sync_xcx',
+                'wifi',
+                '0',
+                'android',
+                new Date().getTime(),
+                'V1_AND_MINISDK_1.5.3_0_RELEASE_B',
+                2001,
+                'null',
+                'anonymous'
+            ]
+            let { data } = await axios.request({
+                baseURL: 'https://q.qq.com/',
+                headers: {
+                    "user-agent": "okhttp/4.4.0"
+                },
+                url: `/report/dc/dc05388`,
+                method: 'post',
+                data: params.join('|')
+            })
+            await new Promise((resolve, reject) => setTimeout(resolve,15*1000))
+        } while (i--)
+
     },
     gameFlowGet: async (axios, options) => {
         // TODO 未知游戏时长上报
