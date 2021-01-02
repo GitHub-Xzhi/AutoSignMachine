dcb6bb164216234ce1ce4436c491e3f5d1c7007a
lunnlew <lunnlew@gmail.com>
2021-01-02 23:40:33 +0800

    update

--- commands/tasks/bilibili/dailyTaskStatus.js
+++ commands/tasks/bilibili/dailyTaskStatus.js
@@ -1,7 +1,7 @@
 module.exports = (axios) => {
   return new Promise((resolve, reject) => {
     axios.request({
-      headers:{
+      headers: {
         "referer": "https://www.bilibili.com",
         "origin": "https://www.bilibili.com",
       },
@@ -10,8 +10,9 @@ module.exports = (axios) => {
       let result = res.data
       if (result.code !== 0) {
         console.log('获取每日任务状态失败', result.message)
+        throw new Error('获取每日任务状态失败:' + result.message)
       } else {
-        console.log('获取每日任务状态成功 ','每日登录+5'+(result.data.login?'完成':'未完成'),'每日观看视频+5'+(result.data.watch?'完成':'未完成'),'每日投币+50'+(result.data.coins?'完成':'未完成'),'每日分享视频+5'+(result.data.share?'完成':'未完成'))
+        console.log('获取每日任务状态成功 ', '每日登录+5' + (result.data.login ? '完成' : '未完成'), '每日观看视频+5' + (result.data.watch ? '完成' : '未完成'), '每日投币+50' + (result.data.coins ? '完成' : '未完成'), '每日分享视频+5' + (result.data.share ? '完成' : '未完成'))
         resolve(result.data)
       }
     }).catch(reject)
