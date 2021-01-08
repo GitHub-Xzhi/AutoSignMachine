9aa0b7ee5a564d778436796ff35e9fe81390fdfc
lunnlew <lunnlew@gmail.com>
2021-01-08 13:53:40 +0800

    update

--- commands/unicom.js
+++ commands/unicom.js
@@ -42,10 +42,10 @@ exports.handler = async function (argv) {
       user: argv.user,
       password: argv.password
     }
-  }).catch(err => console.log("unicom任务:", err.message))
+  }).catch(err => console.log("unicom任务:", err))
   let hasTasks = await scheduler.hasWillTask(command)
   if (hasTasks) {
-    scheduler.execTask(command).catch(err => console.log("unicom任务:", err.message)).finally(() => {
+    scheduler.execTask(command).catch(err => console.log("unicom任务:", err)).finally(() => {
       console.log('全部任务执行完毕！')
     })
   } else {
