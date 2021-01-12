e2e7c9651d52a640a9f3518f7f925be713a2fcd5
lunnlew <lunnlew@gmail.com>
2021-01-12 21:19:18 +0800

    update

--- commands/tasks/unicom/dailyVideoFreeGoods.js
+++ commands/tasks/unicom/dailyVideoFreeGoods.js
@@ -31,21 +31,85 @@ function encryption(data, key) {
 
 var dailyVideoFreeGoods = {
   getGoodsList: async (axios, options) => {
+    const { token } = options
+    let phone = encryption(options.user, 'gb6YCccUvth75Tm2')
     const useragent = `Mozilla/5.0 (Linux; Android 7.1.2; SM-G977N Build/LMY48Z; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36; unicom{version:android@8.0100,desmobile:${options.user}};devicetype{deviceBrand:samsung,deviceModel:SM-G977N};{yw_code:}    `
-    let result = await axios.request({
+    let res = await axios.request({
       headers: {
         "user-agent": useragent,
         "referer": `https://wxapp.msmds.cn/`,
         "origin": "https://wxapp.msmds.cn"
       },
-      url: `https://wxapp.msmds.cn/jplus/api/channel/integral/free/goods/home/list`,
-      method: 'POST'
+      url: `https://wxapp.msmds.cn/jplus/api/channel/integral/free/goods/findAll`,
+      method: 'POST',
+      data: transParams({
+        'fromType': '22',
+        'status': '0',
+        'pageNo': '1',
+        'pageSize': '10',
+        'channelId': 'LT_channel',
+        'phone': phone,
+        'token': token,
+        'sourceCode': 'lt_freeTake'
+      })
     })
-    return result.data.data
+    let result = res.data
+    return {
+      goods: result.data.goodsList.data,
+      freeTimes: result.data.freeTimes,
+      leftTimes: result.data.leftTimes,
+      time: result.data.time,
+      getFreeTime: result.data.getFreeTime,
+      sameGoodsMaxTimes: result.data.sameGoodsMaxTimes,
+    }
   },
   doTask: async (axios, options) => {
-    let phone = encryption(options.user, 'gb6YCccUvth75Tm2')
-    let goods = await dailyVideoFreeGoods.getGoodsList(axios, options)
+    const useragent = `Mozilla/5.0 (Linux; Android 7.1.2; SM-G977N Build/LMY48Z; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36; unicom{version:android@8.0100,desmobile:${options.user}};devicetype{deviceBrand:samsung,deviceModel:SM-G977N};{yw_code:}`
+    let searchParams = {}
+    let result = await axios.request({
+      baseURL: 'https://m.client.10010.com/',
+      headers: {
+        "user-agent": useragent,
+        "referer": `https://img.client.10010.com/`,
+        "origin": "https://img.client.10010.com"
+      },
+      url: `https://m.client.10010.com/mobileService/openPlatform/openPlatLine.htm?to_url=https://wxapp.msmds.cn/h5/react_web/unicom/freeTakePage`,
+      method: 'get',
+      transformResponse: (data, headers) => {
+        if ('location' in headers) {
+          let uu = new URL(headers.location)
+          let pp = {}
+          for (let p of uu.searchParams) {
+            pp[p[0]] = p[1]
+          }
+          if ('ticket' in pp) {
+            searchParams = pp
+          }
+        }
+        return data
+      }
+    }).catch(err => console.log(err))
+
+    let jar1 = result.config.jar
+    let cookiesJson = jar1.toJSON()
+    let ecs_token = cookiesJson.cookies.find(i => i.key == 'ecs_token')
+    ecs_token = ecs_token.value
+    if (!ecs_token) {
+      throw new Error('ecs_token缺失')
+    }
+    let { goods, freeTimes, leftTimes, time, getFreeTime, sameGoodsMaxTimes } = await dailyVideoFreeGoods.getGoodsList(axios, {
+      ...options,
+      token: ecs_token
+    })
+    console.log('签到小游戏买什么都省免费夺宝', `剩余机会(${leftTimes}/${freeTimes})`)
+
+    if (!leftTimes) {
+      if (time) {
+        console.log(`签到小游戏买什么都省免费夺宝: 剩余机会不足，等待下一轮,` + moment().add(time, 'seconds').format('YYYY-MM-DD HH:mm:ss') + ' 后可再次尝试')
+      }
+      throw new Error('签到小游戏买什么都省免费夺宝: 剩余机会不足，等待下一轮')
+    }
+
     let params = {
       'arguments1': '',
       'arguments2': '',
@@ -62,50 +126,14 @@ var dailyVideoFreeGoods = {
     }
     params['sign'] = sign([params.arguments1, params.arguments2, params.arguments3, params.arguments4])
 
-    const useragent = `Mozilla/5.0 (Linux; Android 7.1.2; SM-G977N Build/LMY48Z; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36; unicom{version:android@8.0100,desmobile:${options.user}};devicetype{deviceBrand:samsung,deviceModel:SM-G977N};{yw_code:}`
+    let phone = encryption(options.user, 'gb6YCccUvth75Tm2')
 
-    // 随机商品 似乎限制只能有3次参与机会  这里采用随机商品，每个商品一次的方式
-    let num = 3
-    do {
-      let good = goods[Math.floor(Math.random() * goods.length)]
+    // 同一期商品最多3次机会，每4小时可获得5次机会
+    for (let good of goods) {
 
       console.log('开始处理', good.goodsName)
       params['orderId'] = crypto.createHash('md5').update(new Date().getTime() + '').digest('hex')
       params['arguments4'] = new Date().getTime()
-
-      let searchParams = {}
-      let result = await axios.request({
-        baseURL: 'https://m.client.10010.com/',
-        headers: {
-          "user-agent": useragent,
-          "referer": `https://img.client.10010.com/`,
-          "origin": "https://img.client.10010.com"
-        },
-        url: `https://m.client.10010.com/mobileService/openPlatform/openPlatLine.htm?to_url=https://wxapp.msmds.cn/h5/react_web/unicom/freeTakeGoodDetail/${good.id}?source=unicom`,
-        method: 'get',
-        transformResponse: (data, headers) => {
-          if ('location' in headers) {
-            let uu = new URL(headers.location)
-            let pp = {}
-            for (let p of uu.searchParams) {
-              pp[p[0]] = p[1]
-            }
-            if ('ticket' in pp) {
-              searchParams = pp
-            }
-          }
-          return data
-        }
-      }).catch(err => console.log(err))
-
-      let jar1 = result.config.jar
-
-      let cookiesJson = jar1.toJSON()
-      let ecs_token = cookiesJson.cookies.find(i => i.key == 'ecs_token')
-      ecs_token = ecs_token.value
-      if (!ecs_token) {
-        throw new Error('ecs_token缺失')
-      }
 
       let p = {
         'channelId': 'LT_channel',
@@ -128,30 +156,13 @@ var dailyVideoFreeGoods = {
       })
 
       if (result.data.data.time) {
-        throw new Error(`已处于限制期，` + moment().add(result.data.data.time, 'seconds').format('YYYY-MM-DD HH:mm:ss') + ' 后可再次尝试')
+        throw new Error(`已处于限制期，` + moment().add(result.data.data.time, 'seconds').format('YYYY-MM-DD HH:mm:ss') + ' 后可再次尝试，跳过')
       }
 
       result = await require('./taskcallback').reward(axios, {
         ...options,
         params,
         jar: jar1
-      })
-
-      result = await axios.request({
-        headers: {
-          "user-agent": useragent,
-          "referer": `https://wxapp.msmds.cn/`,
-          "origin": "https://wxapp.msmds.cn"
-        },
-        url: `https://wxapp.msmds.cn/jplus/api/channel/integral/free/goods/goodsInfo`,
-        method: 'POST',
-        data: transParams({
-          'channelId': 'LT_channel',
-          'id': good.id,
-          'phone': phone,
-          'sourceCode': 'lt_freeTake',
-          'token': ecs_token
-        })
       })
 
       timestamp = moment().format('YYYYMMDDHHmmss')
@@ -188,8 +199,7 @@ var dailyVideoFreeGoods = {
       }
       console.log('等待15秒再继续')
       await new Promise((resolve, reject) => setTimeout(resolve, 15 * 1000))
-
-    } while (--num)
+    }
   }
 }
 
