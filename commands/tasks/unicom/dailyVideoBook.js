f20adf3908407411ac12be300091d1e3942806e5
lunnlew <lunnlew@gmail.com>
2021-01-20 11:24:12 +0800

    update

--- commands/tasks/unicom/dailyVideoBook.js
+++ commands/tasks/unicom/dailyVideoBook.js
@@ -221,7 +221,6 @@ var dailyVideoBook = {
   },
   updatePersonReadtime: async (axios, options) => {
     const { detail, m_jar, st_jar } = options
-    const useragent = `Mozilla/5.0 (Linux; Android 7.1.2; SM-G977N Build/LMY48Z; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36; unicom{version:android@8.0100,desmobile:${options.user}};devicetype{deviceBrand:samsung,deviceModel:SM-G977N};{yw_code:}    `
     await dailyVideoBook.ajaxUpdatePersonReadtime(axios, {
       ...options,
       detail,
@@ -595,59 +594,6 @@ var dailyVideoBook = {
   },
   giftBoints: async (axios, options) => {
     const useragent = `Mozilla/5.0 (Linux; Android 7.1.2; SM-G977N Build/LMY48Z; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36; unicom{version:android@8.0100,desmobile:${options.user}};devicetype{deviceBrand:samsung,deviceModel:SM-G977N};{yw_code:}    `
-    // let Authorization = await dailyVideoBook.oauthMethod(axios, options)
-    // let { Token, m_jar, st_jar } = await dailyVideoBook.login(axios, {
-    //   ...options,
-    //   Authorization
-    // })
-    // let cardid = '11910'
-    // console.log('取得书籍列表')
-    // let books = await dailyVideoBook.getBookList(axios, {
-    //   ...options,
-    //   jar: st_jar,
-    //   params: {
-    //     cardid
-    //   }
-    // })
-    // let book = books[Math.floor(Math.random() * books.length)]
-    // console.log('随机获取', book.cntname)
-
-    // let chapter = {
-    //   chapterseno: 1
-    // }
-
-    // let chapters = await dailyVideoBook.getBookUpDownChapter(axios, {
-    //   ...options,
-    //   jar: st_jar,
-    //   book,
-    //   chapter
-    // })
-
-    // chapter = chapters[0]
-
-    // let detail = {
-    //   'cntindex': book.cntindex,
-    //   'catid': '118440',
-    //   'pageIndex': '10843',
-    //   'cardid': cardid,
-    //   'desmobile': options.user,
-    //   'version': 'android@8.0100',
-    //   'cntname': book.cntname,
-    //   'channelid': '18000018',
-    //   'chapterallindex': chapter.chapterallindex,
-    //   'volumeallindex': chapter.volumeallindex,
-    //   'chapterseno': chapter.chapterseno,
-    //   'cntid': chapter.cntid,
-    //   'cnttype': book.cnttype
-    // }
-
-    // await dailyVideoBook.updatePersonReadtime(axios, {
-    //   ...options,
-    //   detail,
-    //   st_jar: st_jar,
-    //   m_jar: m_jar
-    // })
-
     let res = await axios.request({
       headers: {
         "user-agent": useragent,
