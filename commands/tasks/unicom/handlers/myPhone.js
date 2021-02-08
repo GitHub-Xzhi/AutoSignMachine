e70455f2865b666a73685a93f97dcb4fffa54150
cooip-jm <78735965+cooip-jm@users.noreply.github.com>
2021-02-08 21:12:13 +0800

    Update myPhone.js

--- commands/tasks/unicom/handlers/myPhone.js
+++ commands/tasks/unicom/handlers/myPhone.js
@@ -1,7 +1,10 @@
 const useragent = (options) =>
   `Mozilla/5.0 (Linux; Android 7.1.2; SM-G977N Build/LMY48Z; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36; unicom{version:android@8.0102,desmobile:${options.user}};devicetype{deviceBrand:samsung,deviceModel:SM-G977N};{yw_code:}`;
 //   `Mozilla/5.0 (Linux; Android 7.1.2; SM-G977N Build/LMY48Z; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36;devicetype{deviceBrand:samsung,deviceModel:SM-G977N};{yw_code:};unicom{version:android@8.0102,desmobile:${options.user}}`;
-// const USER_AGENTS = [];
+// const USER_AGENTS = [
+//  'Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148   unicom{version:iphone_c@8.0102}{systemVersion:dis}{yw_code:}',
+//  'Mozilla/5.0 (iPhone; CPU iPhone OS 12_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 unicom{version:iphone_c@8.0102}{systemVersion:dis}{yw_code:}'
+//];
 
 function randomNumber(min = 0, max = 100) {
   return Math.min(Math.floor(min + Math.random() * (max - min)), max);
