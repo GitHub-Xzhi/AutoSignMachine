c4232658fab799a2b8f2111b8f3ec9bcd1597c63
clamp <15528374+DJ-clamp@users.noreply.github.com>
2021-02-08 15:18:05 +0800

    🚧 creating a new game event

--- commands/tasks/unicom/bcow.js
+++ commands/tasks/unicom/bcow.js
@@ -1,92 +1,26 @@
-var crypto = require("crypto");
-var CryptoJS = require("crypto-js");
-var moment = require("moment");
-
-// 摇一摇送好礼
-let transParams = (data) => {
-  let params = new URLSearchParams();
-  for (let item in data) {
-    params.append(item, data["" + item + ""]);
-  }
-  return params;
-};
-
-var sign = (data) => {
-  let str = "integralofficial&";
-  let params = [];
-  data.forEach((v, i) => {
-    if (v) {
-      params.push("arguments" + (i + 1) + v);
-    }
-  });
-  return crypto
-    .createHash("md5")
-    .update(str + params.join("&"))
-    .digest("hex");
-};
-// prettier-ignore
-let charMap =  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E",
-"F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X",
-"Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
-"r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-var secretkeyArray = function () {
-  for (let e = [], t = charMap, i = 0; 5 > i; i++) {
-    for (let n = "", s = 0; 16 > s; s++) {
-      let a = Math.floor(62 * Math.random());
-      n += t[a];
-    }
-    e.push(n);
-  }
-  return e;
-};
-
-var newjiamarr = () => {
-  for (let e = [], k = "", t = charMap, i = 0x0; 0x4 > i; i++) {
-    for (let n = "", s = 0x0; 16 > s; s++) {
-      let a = Math.floor(0x3e * Math.random());
-      n += t[a];
-    }
-    e.push(n), (k += n.substring(0x0, 0x4));
-  }
-  return {
-    arr: e,
-    zfc: k,
-  };
-};
-
-var encrypt = function (word, keyStr) {
-  var key = CryptoJS.enc.Utf8.parse(keyStr);
-  var srcs = CryptoJS.enc.Utf8.parse(word);
-  var encrypted = CryptoJS.AES.encrypt(srcs, key, {
-    mode: CryptoJS.mode.ECB,
-    padding: CryptoJS.pad.Pkcs7,
-  });
-  return encrypted.toString();
-};
-
-var decrypt = function (word, keyStr) {
-  var key = CryptoJS.enc.Utf8.parse(keyStr);
-  var decrypted = CryptoJS.AES.decrypt(word, key, {
-    mode: CryptoJS.mode.ECB,
-    padding: CryptoJS.pad.Pkcs7,
-  });
-  return decrypted.toString(CryptoJS.enc.Utf8);
-};
-
-var dailyYYY = {
+let crypto = require("crypto");
+let CryptoJS = require("crypto-js");
+let moment = require("moment");
+let AES = require("./handlers/PAES");
+const useragent = (options) =>
+  `Mozilla/5.0 (Linux; Android 7.1.2; SM-G977N Build/LMY48Z; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36; unicom{version:android@8.0102,desmobile:${options.user}};devicetype{deviceBrand:samsung,deviceModel:SM-G977N};{yw_code:}`;
+module.exports = bcow = {
   doTask: async (axios, options) => {
-    const useragent = `Mozilla/5.0 (Linux; Android 7.1.2; SM-G977N Build/LMY48Z; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36; unicom{version:android@8.0100,desmobile:${options.user}};devicetype{deviceBrand:samsung,deviceModel:SM-G977N};{yw_code:}`;
+    console.log("🔔开始抽牛卡\n");
+    let cookies = await getOpenPlatLine(axios, options);
+    let data = await postFreeLoginRock(axios, options, cookies);
+  },
+  getOpenPlatLine: async (axios, options) => {
     let searchParams = {};
     let result = await axios
       .request({
         baseURL: "https://m.client.10010.com/",
         headers: {
-          "user-agent": useragent,
+          "user-agent": useragent(options),
           referer: `https://img.client.10010.com/`,
           origin: "https://img.client.10010.com",
         },
-        url: `https://m.client.10010.com/mobileService/openPlatform/openPlatLine.htm?to_url=https://m.jf.10010.com/jf-order/avoidLogin/forActive/yyyqd&duanlianjieabc=tbkwx`,
+        url: `https://m.client.10010.com/mobileService/openPlatform/openPlatLine.htm?to_url=https://m.jf.10010.com/jf-order/avoidLogin/forActive/ncow&duanlianjieabc=tbLlf`,
         method: "GET",
         transformResponse: (data, headers) => {
           if ("location" in headers) {
@@ -106,6 +40,7 @@ var dailyYYY = {
     let jar1 = result.config.jar;
 
     let cookiesJson = jar1.toJSON();
+    console.log(cookiesJson);
     let ecs_token = cookiesJson.cookies.find((i) => i.key == "ecs_token");
     if (!ecs_token) {
       throw new Error("ecs_token缺失");
@@ -116,33 +51,30 @@ var dailyYYY = {
       throw new Error("jfid缺失");
     }
     jfid = jfid.value;
-
-    // let ff = { "params": "vogRkS7brjqaInNHJv4oumE6HtGp7tbG7ZE9P21hmoaBq/rSwtvFjI/vcw2bNbdFUW1s27XOdaXbcBWHpgDYxmQuMP4aFj0Fy3JYp566sCH+97rQyrJRf1GzslAsSiLQJC4DQawzH+J54FIipKElMw==4", "parKey": ["YTJapEBcsMsMOU1i", "yJI3cV3zUxqZWd5j", "TmLBjGV8hTfZ7rMf", "QqAuodE1Zz9eA84p", "IM9f9CfT8LDK2RQ7"] }
-    // console.log(decrypt(ff.params.substr(0, ff.params.length - 1), ff.parKey[parseInt(ff.params.substr(-1, 1))]))
-    // process.exit(0)
-    let keyArr = secretkeyArray();
+    return { jfid, searchParams, jar1 };
+  },
+  postFreeLoginRock: async (axios, options, { jfid, searchParams, jar1 }) => {
+    let keyArr = AES.secretkeyArray();
     let keyrdm = Math.floor(Math.random() * 5);
 
     let params = {
-      activityId: "Ac-9b71780cb87844b9ac3ab5d34b11dd24",
+      activityId: "Ac-yccnk",
       userCookie: jfid,
       userNumber: searchParams.userNumber,
       time: new Date().getTime(),
     };
-
     let reqdata = {
       params: encrypt(JSON.stringify(params), keyArr[keyrdm]) + keyrdm,
       parKey: keyArr,
     };
-
     //foods list
     let res = await axios
       .request({
         baseURL: "https://m.jf.10010.com/",
         headers: {
-          "user-agent": useragent,
+          "user-agent": useragent(options),
           referer:
-            "https://m.jf.10010.com/cms/yuech/unicom-integral-ui/yuech-Blindbox/shake/index.html?jump=sign",
+            "https://m.jf.10010.com/cms/yuech/unicom-integral-ui/yuech-qd/bcow/index.html?jump=sign",
           origin: "https://img.jf.10010.com",
           "Content-Type": "application/json",
         },
@@ -157,12 +89,18 @@ var dailyYYY = {
     if (result.code !== 0) {
       throw new Error(result.message);
     }
-
     let activity = result.data.activityInfos.activityVOs[0]; //available items on the list from request
     let Authorization = result.data.token.access_token;
     let freeTimes = activity.activityTimesInfo.freeTimes;
     let advertTimes = activity.activityTimesInfo.advertTimes;
-
+    return { activity, Authorization, freeTimes, advertTimes };
+  },
+  postTimesDrawForPrize: async (
+    axios,
+    options,
+    { jfid, searchParams, jar1 },
+    { activity, Authorization, freeTimes, advertTimes }
+  ) => {
     do {
       let orderId = "";
 
@@ -188,20 +126,21 @@ var dailyYYY = {
         type: "免费",
       };
 
+      //check game time information
       if (!freeTimes && advertTimes) {
         let params = {
           arguments1: "AC20200611152252",
-          arguments2: "",
-          arguments3: "",
+          arguments2: "GGPD",
+          arguments3: "627292f1243148159c58fd58917c3e67",
           arguments4: new Date().getTime(),
-          arguments6: "",
-          arguments7: "",
-          arguments8: "",
-          arguments9: "",
+          arguments6: "517050707",
+          arguments7: "517050707",
+          arguments8: "123456",
+          arguments9: "4640b530b3f7481bb5821c6871854ce5",
           netWay: "Wifi",
-          remark1: "签到小游戏摇摇乐不倒翁",
-          remark: "签到小游戏翻倍得积分",
-          version: `android@8.0100`,
+          remark1: "签到翻牛牌活动",
+          remark: "签到看视频翻倍得积分",
+          version: `android@8.0102`,
           codeId: 945689604,
         };
         params["sign"] = sign([
@@ -209,6 +148,10 @@ var dailyYYY = {
           params.arguments2,
           params.arguments3,
           params.arguments4,
+          params.arguments6,
+          params.arguments7,
+          params.arguments8,
+          params.arguments9,
         ]);
         params["orderId"] = crypto
           .createHash("md5")
@@ -225,10 +168,10 @@ var dailyYYY = {
         let timestamp = moment().format("YYYYMMDDHHmmss");
         result = await axios.request({
           headers: {
-            "user-agent": useragent,
+            "user-agent": useragent(options),
             referer: `https://img.client.10010.com/`,
           },
-          url: `https://m.jf.10010.com/jf-order/avoidLogin/forActive/yyyqd?ticket=${searchParams.ticket}&type=02&version=android@8.0100&timestamp=${timestamp}&desmobile=${options.user}&num=0&postage=${searchParams.postage}&duanlianjieabc=tbkwx&userNumber=${options.user}`,
+          url: `https://m.jf.10010.com/jf-order/avoidLogin/forActive/ncow?ticket=${searchParams.ticket}&type=02&version=android@8.0102&timestamp=${timestamp}&desmobile=${options.user}&num=0&postage=${searchParams.postage}&duanlianjieabc=tbLlf&userNumber=${options.user}`,
           method: "GET",
         });
 
@@ -241,12 +184,12 @@ var dailyYYY = {
           version: "8.0102",
         };
         advertTimes--;
-
         orderId = params["orderId"];
       } else {
         freeTimes--;
       }
 
+      //join the game
       let n = Math.floor(5 * Math.random());
       let i = newjiamarr();
       params = {
@@ -256,32 +199,20 @@ var dailyYYY = {
 
       res = await axios
         .request({
+          baseURL: "https://m.jf.10010.com/",
           headers: {
             Authorization: `Bearer ${Authorization}`,
-            "user-agent": useragent,
+            "user-agent": useragent(options),
             referer:
-              "https://m.jf.10010.com/cms/yuech/unicom-integral-ui/yuech-Blindbox/shake/index.html?jump=sign",
+              "https://m.jf.10010.com/cms/yuech/unicom-integral-ui/yuech-qd/bcow/index.html?jump=sign",
             origin: "https://m.jf.10010.com",
-            "Content-Type": "application/json",
+            "Content-Type": "application/json;charset=UTF-8",
           },
-          url: `https://m.jf.10010.com/jf-yuech/api/gameResultV2/minusGameTimes`,
+          url: `/jf-yuech/api/gameResultV2/timesDrawForPrize`,
           method: "post",
           data: params,
         })
         .catch((err) => console.log(err));
-      //deprecated from official API call
-      // res = await axios.request({
-      //   headers: {
-      //     "user-agent": useragent,
-      //     "Authorization": `Bearer ${Authorization}`,
-      //     "referer": "https://m.jf.10010.com/cms/yuech/unicom-integral-ui/yuech-Blindbox/shake/index.html?jump=sign",
-      //     "Content-Type": "application/x-www-form-urlencoded"
-      //   },
-      //   jar: null,
-      //   url: `https://m.jf.10010.com/jf-yuech/api/gameResultV2/consumptionGameTimes`,
-      //   method: 'get',
-      //   params: transParams(p1)
-      // }).catch(err => console.log(err))
 
       console.log(res.data);
       if (res.data.code !== 0) {
@@ -292,43 +223,17 @@ var dailyYYY = {
         }
       }
 
-      let t = {
-        activityId: activity.activityId,
-        resultId: res.data.data.resultId,
-      };
-
-      // n = Math.floor(5 * Math.random())
-      i = newjiamarr();
-      params = {
-        params: encrypt(JSON.stringify(t), i["zfc"]) + n,
-        parKey: i["arr"],
-      };
-      res = await axios.request({
-        headers: {
-          Authorization: `Bearer ${Authorization}`,
-          "user-agent": useragent,
-          referer:
-            "https://m.jf.10010.com/cms/yuech/unicom-integral-ui/yuech-Blindbox/shake/index.html?jump=sign",
-          origin: "https://m.jf.10010.com",
-          "Content-Type": "application/json;charset=UTF-8",
-        },
-        url: `https://m.jf.10010.com/jf-yuech/api/gameResultV2/luckDrawForPrize`,
-        method: "post",
-        data: params,
-      });
-
+      //TODO: coding in here now...
       result = res.data;
       if (result.code !== 0) {
-        console.log("摇一摇送好礼:", result.message);
+        console.log("翻牛牌送好礼:", result.message);
       } else {
         console.log(
-          "摇一摇送好礼:",
+          "翻牛牌送好礼:",
           result.data.status === "中奖"
             ? result.data.prizeName
             : result.data.status
         );
-
-        //TODO: coding in here now...
         if (result.data.doublingStatus) {
           console.log("提交积分翻倍");
           await dailyYYY.lookVideoDouble(axios, {
@@ -342,20 +247,23 @@ var dailyYYY = {
           });
         }
       }
-
-      console.log("等待15秒再继续");
-      await new Promise((resolve, reject) => setTimeout(resolve, 15 * 1000));
     } while (freeTimes || advertTimes);
   },
   lookVideoDouble: async (axios, options) => {
     let params = {
-      arguments1: "AC20200611152252", // acid
-      arguments2: "GGPD", // yhChannel
-      arguments3: "627292f1243148159c58fd58917c3e67", // yhTaskId menuId
-      arguments4: new Date().getTime(), // time
+      arguments1: "AC20200611152252",
+      arguments2: "GGPD",
+      arguments3: "627292f1243148159c58fd58917c3e67",
+      arguments4: new Date().getTime(),
       arguments6: "517050707",
+      arguments7: "517050707",
+      arguments8: "123456",
+      arguments9: "4640b530b3f7481bb5821c6871854ce5",
       netWay: "Wifi",
-      version: `android@8.0100`,
+      remark1: "签到翻牛牌活动",
+      remark: "签到看视频翻倍得积分",
+      version: `android@8.0102`,
+      codeId: 945689604,
     };
     params["sign"] = sign([
       params.arguments1,
@@ -369,7 +277,7 @@ var dailyYYY = {
     });
 
     if (!num) {
-      console.log("签到小游戏摇摇乐不倒翁: 今日已完成");
+      console.log("签到小游戏翻牛牌: 今日已完成");
       return;
     }
 
@@ -387,7 +295,7 @@ var dailyYYY = {
         .update(new Date().getTime() + "")
         .digest("hex"),
       netWay: "Wifi",
-      remark: "签到小游戏摇摇乐不倒翁",
+      remark: "签到小游戏翻牛牌",
       version: `android@8.0100`,
       codeId: 945689604,
     };
@@ -418,11 +326,9 @@ var dailyYYY = {
     });
     result = res.data;
     if (result.code !== 0) {
-      console.log("摇一摇送好礼翻倍结果:", result.message);
+      console.log("翻牛牌送好礼翻倍结果:", result.message);
     } else {
-      console.log("摇一摇送好礼翻倍结果:", result.data);
+      console.log("翻牛牌送好礼翻倍结果:", result.data);
     }
   },
 };
-
-module.exports = dailyYYY;
--- commands/tasks/unicom/unicom.js
+++ commands/tasks/unicom/unicom.js
@@ -1,80 +1,123 @@
-const { scheduler } = require('../../../utils/scheduler')
-const { getCookies, saveCookies } = require('../../../utils/util')
-const _request = require('../../../utils/request')
+const { scheduler } = require("../../../utils/scheduler");
+const { getCookies, saveCookies } = require("../../../utils/util");
+const _request = require("../../../utils/request");
 
 var start = async (params) => {
-  const { cookies, options } = params
+  const { cookies, options } = params;
 
   let init = async (request, savedCookies) => {
-    await require('./init')(request, {
+    await require("./init")(request, {
       ...params,
-      cookies: savedCookies || cookies
-    })
+      cookies: savedCookies || cookies,
+    });
     return {
-      request
-    }
-  }
+      request,
+    };
+  };
   let taskOption = {
-    init
-  }
+    init,
+  };
 
   // 每日签到积分
-  await scheduler.regTask('dailysignin', async (request) => {
-    await require('./dailysignin').doTask(request, options)
-    await require('./integral').addFlow(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "dailysignin",
+    async (request) => {
+      await require("./dailysignin").doTask(request, options);
+      await require("./integral").addFlow(request, options);
+    },
+    taskOption
+  );
 
   // 冬奥积分活动 20201231
-  await scheduler.regTask('winterTwo', async (request) => {
-    await require('./integral').winterTwoGetIntegral(request, options)
-    await require('./integral').winterTwoStatus(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "winterTwo",
+    async (request) => {
+      await require("./integral").winterTwoGetIntegral(request, options);
+      await require("./integral").winterTwoStatus(request, options);
+    },
+    taskOption
+  );
 
   // 每日定向积分 20201231
-  await scheduler.regTask('dxIntegralEveryDay', async (request) => {
-    await require('./integral').dxIntegralEveryDay(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "dxIntegralEveryDay",
+    async (request) => {
+      await require("./integral").dxIntegralEveryDay(request, options);
+    },
+    taskOption
+  );
 
   // 每日游戏楼层宝箱
-  await scheduler.regTask('dailygamebox', async (request) => {
-    await require('./integral').gamebox(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "dailygamebox",
+    async (request) => {
+      await require("./integral").gamebox(request, options);
+    },
+    taskOption
+  );
 
   // 每日抽奖
-  await scheduler.regTask('dailylotteryintegral', async (request) => {
-    await require('./dailylotteryintegral').doTask(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "dailylotteryintegral",
+    async (request) => {
+      await require("./dailylotteryintegral").doTask(request, options);
+    },
+    taskOption
+  );
 
   // 首页-游戏-娱乐中心-沃之树
-  await scheduler.regTask('dailywoTree', async (request) => {
-    await require('./woTree').doTask(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "dailywoTree",
+    async (request) => {
+      await require("./woTree").doTask(request, options);
+    },
+    taskOption
+  );
 
-  await scheduler.regTask('dailyBookRead', async (request) => {
-    // 首页-小说-阅读越有礼打卡赢话费
-    await require('./dailyBookRead').doTask(request, options)
-    await require('./dailyVideoBook').doTask(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "dailyBookRead",
+    async (request) => {
+      // 首页-小说-阅读越有礼打卡赢话费
+      await require("./dailyBookRead").doTask(request, options);
+      await require("./dailyVideoBook").doTask(request, options);
+    },
+    taskOption
+  );
 
   // 首页-小说-读满10章赢好礼
-  await scheduler.regTask('dailyBookRead10doDraw', async (request) => {
-    // 首页-小说-读满10章赢好礼
-    await require('./dailyVideoBook').read10doDraw(request, options)
-    // 首页-小说-读满10章赢好礼-看视频领2积分
-    await require('./dailyVideoBook').read10doDrawLookVideoDouble(request, options)
-    // 首页-签到有礼-免流量得福利-3积分天天拿(阅读打卡)
-    await require('./dailyVideoBook').giftBoints(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "dailyBookRead10doDraw",
+    async (request) => {
+      // 首页-小说-读满10章赢好礼
+      await require("./dailyVideoBook").read10doDraw(request, options);
+      // 首页-小说-读满10章赢好礼-看视频领2积分
+      await require("./dailyVideoBook").read10doDrawLookVideoDouble(
+        request,
+        options
+      );
+      // 首页-签到有礼-免流量得福利-3积分天天拿(阅读打卡)
+      await require("./dailyVideoBook").giftBoints(request, options);
+    },
+    taskOption
+  );
 
-  await scheduler.regTask('dailyBookLuckdraw', async (request) => {
-    // 首页-小说-阅读福利抽大奖
-    await require('./dailyBookLuckdraw').doTask(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "dailyBookLuckdraw",
+    async (request) => {
+      // 首页-小说-阅读福利抽大奖
+      await require("./dailyBookLuckdraw").doTask(request, options);
+    },
+    taskOption
+  );
 
   // 首页-签到有礼-免费领-浏览领积分
-  await scheduler.regTask('dailyLiuLan', async (request) => {
-    await require('./dailyTTliulan').doTask(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "dailyLiuLan",
+    async (request) => {
+      await require("./dailyTTliulan").doTask(request, options);
+    },
+    taskOption
+  );
 
   // 首页-签到有礼-免费拿-看视频夺宝
   // 易出现本次操作需要进行验证，暂时注释
@@ -87,82 +130,134 @@ var start = async (params) => {
   // })
 
   // 首页-签到有礼-免费抽-抓OPPO手机
-  await scheduler.regTask('dailyGrabdollPage', async (request) => {
-    await require('./dailyGrabdollPage').doTask(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "dailyGrabdollPage",
+    async (request) => {
+      await require("./dailyGrabdollPage").doTask(request, options);
+    },
+    taskOption
+  );
 
   // 首页-签到有礼-免费抽-拿666积分-豪礼大派送抽奖
-  await scheduler.regTask('jflottery', async (request) => {
-    await require('./jflottery').timesDraw(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "jflottery",
+    async (request) => {
+      await require("./jflottery").timesDraw(request, options);
+    },
+    taskOption
+  );
 
   // 首页-签到有礼-免费抽-拿苹果iPad Pro(摇一摇)
-  await scheduler.regTask('dailyYYY', async (request) => {
-    await require('./dailyYYY').doTask(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "dailyYYY",
+    async (request) => {
+      await require("./dailyYYY").doTask(request, options);
+    },
+    taskOption
+  );
 
   // 首页-签到有礼-免费抽-华为mate40pro(刮刮乐)
-  await scheduler.regTask('dailyVideoScratchcard', async (request) => {
-    await require('./dailyVideoScratchcard').doTask(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "dailyVideoScratchcard",
+    async (request) => {
+      await require("./dailyVideoScratchcard").doTask(request, options);
+    },
+    taskOption
+  );
 
-  // 首页-签到有礼-免费抽-赢三星Galaxy Z(试试手气)
-  await scheduler.regTask('dailyCheapStorePage', async (request) => {
-    await require('./dailyCheapStorePage').doTask(request, options)
-  }, {
-    isCircle: true,
-    intervalTime: 4 * 3600,
-    ...taskOption
-  })
+  // 首页-签到有礼-免费抽-赢三星Galaxy Z(试试手气) deprecated from official API.
+  // await scheduler.regTask('dailyCheapStorePage', async (request) => {
+  //   await require('./dailyCheapStorePage').doTask(request, options)
+  // }, {
+  //   isCircle: true,
+  //   intervalTime: 4 * 3600,
+  //   ...taskOption
+  // })
 
   // 首页-签到有礼-免费抽-拆华为Pad(去抽奖)
-  await scheduler.regTask('dailyLKMH', async (request) => {
-    await require('./dailyLKMH').doTask(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "dailyLKMH",
+    async (request) => {
+      await require("./dailyLKMH").doTask(request, options);
+    },
+    taskOption
+  );
 
   // 首页-签到有礼-免费抽-拿iPhone12(摇一摇)
-  await scheduler.regTask('dailyYYQ', async (request) => {
-    await require('./dailyYYQ').doTask(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "dailyYYQ",
+    async (request) => {
+      await require("./dailyYYQ").doTask(request, options);
+    },
+    taskOption
+  );
 
   // 首页-签到有礼-免费抽-赢Apple Watch(去抽奖)
-  await scheduler.regTask('dailyTurntablePage', async (request) => {
-    await require('./dailyTurntablePage').doTask(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "dailyTurntablePage",
+    async (request) => {
+      await require("./dailyTurntablePage").doTask(request, options);
+    },
+    taskOption
+  );
 
   // 首页-签到有礼-赚更多福利-看视频奖励5积分
-  await scheduler.regTask('dailyVideo', async (request) => {
-    await require('./dailyVideo').doTask(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "dailyVideo",
+    async (request) => {
+      await require("./dailyVideo").doTask(request, options);
+    },
+    taskOption
+  );
 
   // 首页-签到有礼-赚更多福利-天天抽好礼
-  await scheduler.regTask('dailylottery', async (request) => {
-    await require('./dailylottery').doTask(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "dailylottery",
+    async (request) => {
+      await require("./dailylottery").doTask(request, options);
+    },
+    taskOption
+  );
 
   // 首页-游戏-娱乐中心-每日打卡
-  await scheduler.regTask('producGameSignin', async (request) => {
-    await require('./producGame').gameSignin(request, options)
-    await require('./producGame').gameBox(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "producGameSignin",
+    async (request) => {
+      await require("./producGame").gameSignin(request, options);
+      await require("./producGame").gameBox(request, options);
+    },
+    taskOption
+  );
 
   // 首页-游戏-娱乐中心-天天领取3G流量包
-  await scheduler.regTask('dailygameflow', async (request) => {
-    await require('./producGame').doGameFlowTask(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "dailygameflow",
+    async (request) => {
+      await require("./producGame").doGameFlowTask(request, options);
+    },
+    taskOption
+  );
 
   // 首页-积分查询-游戏任务
-  await scheduler.regTask('dailygameIntegral', async (request) => {
-    await require('./producGame').doGameIntegralTask(request, options)
-  }, taskOption)
+  await scheduler.regTask(
+    "dailygameIntegral",
+    async (request) => {
+      await require("./producGame").doGameIntegralTask(request, options);
+    },
+    taskOption
+  );
 
   // 首页-知识-限时免费（连续7天阶梯激励）
-  await scheduler.regTask('dailyCourse', async (request) => {
-    await require('./dailyCourse').doTask(request, options)
-  }, {
-    ...taskOption,
-    startTime: 9 * 3600
-  })
+  await scheduler.regTask(
+    "dailyCourse",
+    async (request) => {
+      await require("./dailyCourse").doTask(request, options);
+    },
+    {
+      ...taskOption,
+      startTime: 9 * 3600,
+    }
+  );
 
   // await require('./integral').getflDetail(request, options)
   // await require('./integral').getTxDetail(request, options)
@@ -170,51 +265,81 @@ var start = async (params) => {
   // await require('./integral').getCoins(request, options)
 
   // 每日评论积分
-  await scheduler.regTask('dailycomment', async (request) => {
-    await require('./commentSystem').commentTask(request, options).catch(console.log)
-  }, taskOption)
+  await scheduler.regTask(
+    "dailycomment",
+    async (request) => {
+      await require("./commentSystem")
+        .commentTask(request, options)
+        .catch(console.log);
+    },
+    taskOption
+  );
 
   // 首页-游戏-娱乐中心-每日打卡-完成今日任务(200m)
-  await scheduler.regTask('todayDailyTask', async (request) => {
-    await require('./producGame').doTodayDailyTask(request, options)
-  }, {
-    ...taskOption,
-    startTime: 20 * 3600
-  })
+  await scheduler.regTask(
+    "todayDailyTask",
+    async (request) => {
+      await require("./producGame").doTodayDailyTask(request, options);
+    },
+    {
+      ...taskOption,
+      startTime: 20 * 3600,
+    }
+  );
 
   // 首页-签到有礼-居家娱乐馆
-  await scheduler.regTask('gameYearBox', async (request) => {
-    await require('./gameYearBox').doTask(request, options)
-  }, {
-    ...taskOption,
-    startTime: 20 * 3600
-  })
+  await scheduler.regTask(
+    "gameYearBox",
+    async (request) => {
+      await require("./gameYearBox").doTask(request, options);
+    },
+    {
+      ...taskOption,
+      startTime: 20 * 3600,
+    }
+  );
 
   // 首页-牛气-秒杀抢兑
-  await scheduler.regTask('NiujieSpikePrize', async (request) => {
-    await require('./Niujie').spikePrize(request, options)
-  }, {
-    ...taskOption,
-    startTime: 9.6 * 3600,
-    ignoreRelay: true
-  })
+  await scheduler.regTask(
+    "NiujieSpikePrize",
+    async (request) => {
+      await require("./Niujie").spikePrize(request, options);
+    },
+    {
+      ...taskOption,
+      startTime: 9.6 * 3600,
+      ignoreRelay: true,
+    }
+  );
 
   // 首页-牛气-转盘抽奖
-  await scheduler.regTask('NiujieTask', async (request) => {
-    await require('./Niujie').doTask(request, options)
-  }, taskOption)
-
+  await scheduler.regTask(
+    "NiujieTask",
+    async (request) => {
+      await require("./Niujie").doTask(request, options);
+    },
+    taskOption
+  );
 
   // 首页-牛气-场馆领牛气
-  await scheduler.regTask('NiujieReceiveCalf', async (request) => {
-    await require('./Niujie').receiveCalf(request, options)
-  }, {
-    isCircle: true,
-    intervalTime: 1 * 3600,
-    startTime: 1,
-    ...taskOption
-  })
-}
+  await scheduler.regTask(
+    "NiujieReceiveCalf",
+    async (request) => {
+      await require("./Niujie").receiveCalf(request, options);
+    },
+    {
+      isCircle: true,
+      intervalTime: 1 * 3600,
+      startTime: 1,
+      ...taskOption,
+    }
+  );
+
+  // 首页-签到有礼-赢vivo X60
+  // await scheduler.regTask('bcow', async (request) => {
+  //   await require('./bcow').doTask(request, options)
+  // }, taskOption)
+};
 module.exports = {
-  start
-}
+  start,
+};
